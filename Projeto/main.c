b13371c6957085dd847a10fdb464f43cc0efaca0
jeajjr <jeajjr@hotmail.com>
2016-02-29 21:28:46 -0300

    Removed test and old codes

--- Projeto/main.c
+++ Projeto/main.c
@@ -10,6 +10,9 @@
 #include "driverlib/debug.h"
 #include "driverlib/sysctl.h"
 #include "driverlib/adc.h"
+#include "driverlib/timer.h"
+
+#include "osciloscopio.h"
 
 #define PI 3.141592
 
@@ -19,8 +22,16 @@ void__error__(char *pcFilename, unsigned long ulLine)
 }
 #endif
 
+/**
+ * GLOBAL VARIABLES
+ */
 int LED = 0;
+unsigned char current_trigger_level = (unsigned char) ((TRIGGER_LEVEL_100 + TRIGGER_LEVEL_0) / 2);
+unsigned char current_time_scale = TIME_SCALE_1MS;
 
+/**
+ * INTERRUPT HANDLERS
+ */
 void UART0IntHandler(void)
 {
 	unsigned long ulStatus;
@@ -29,28 +40,23 @@ void UART0IntHandler(void)
 	while(UARTCharsAvail(UART0_BASE)) //loop while there are chars
 	{
 		char a = UARTCharGetNonBlocking(UART0_BASE);
-		UARTCharPutNonBlocking(UART1_BASE, a);
 	}
 }
 
 void UART1IntHandler(void)
 {
 	unsigned long ulStatus;
-		ulStatus = UARTIntStatus(UART1_BASE, true); //get interrupt status
-		UARTIntClear(UART1_BASE, ulStatus); //clear the asserted interrupts
-		while(UARTCharsAvail(UART1_BASE)) //loop while there are chars
-		{
-			char a = UARTCharGetNonBlocking(UART1_BASE);
-			UARTCharPutNonBlocking(UART0_BASE, a);
-
-			LED++;
-			if (LED%2) //if (val == 0)
-				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 1<<3);
-			else //if (val == 255)
-				GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-		}
+	ulStatus = UARTIntStatus(UART1_BASE, true); //get interrupt status
+	UARTIntClear(UART1_BASE, ulStatus); //clear the asserted interrupts
+	while(UARTCharsAvail(UART1_BASE)) //loop while there are chars
+	{
+		char a = UARTCharGetNonBlocking(UART1_BASE);
+	}
 }
 
+/**
+ * MAIN METHOD
+ */
 int main(void)
 {
 	unsigned long ulADC0Value[4];
@@ -109,96 +115,27 @@ int main(void)
 	UARTIntEnable(UART1_BASE, UART_INT_RX | UART_INT_RT);
 */
 
-
 	GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 1<<3);
-/*
-	UARTCharPut(UART0_BASE, 'E');
-	UARTCharPut(UART0_BASE, 'n');
-	UARTCharPut(UART0_BASE, 't');
-	UARTCharPut(UART0_BASE, 'e');
-	UARTCharPut(UART0_BASE, 'r');
-	UARTCharPut(UART0_BASE, ' ');
-	UARTCharPut(UART0_BASE, 'T');
-	UARTCharPut(UART0_BASE, 'e');
-	UARTCharPut(UART0_BASE, 'x');
-	UARTCharPut(UART0_BASE, 't');
-	UARTCharPut(UART0_BASE, ':');
-	UARTCharPut(UART0_BASE, ' ');
-*/
 	GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 1<<2);
 
-	char val = 0;
-	int sampleCount = 0;
 	int limLED = 10000;
 
 	while(1)
 	{
-//#ifdef NOIA
 		ADCIntClear(ADC0_BASE, 1);
 		ADCProcessorTrigger(ADC0_BASE, 1);
 		while(!ADCIntStatus(ADC0_BASE, 1, false));
 		ADCSequenceDataGet(ADC0_BASE, 1, ulADC0Value);
 		unsigned int leitura = (ulADC0Value[0] + ulADC0Value[1] + ulADC0Value[2] + ulADC0Value[3])/4;
-/*
-		ADCSequenceDataGet(ADC0_BASE, 3, ulADC0Value);
-		unsigned int leitura = ulADC0Value[0];
-		/*
-		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 1<<1);
-		UARTCharPut(UART0_BASE, (leitura>>4) & 0xFF);
-		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0);
-		*/
-		//UARTCharPut(UART0_BASE, leitura & 0xFF);
 
-		//SysCtlDelay(200000);
-//		if (sampleCount++ == 10)
 		{
 			UARTCharPut(UART0_BASE, (leitura>>4) & 0xFF);
 			sampleCount = 0;
 		}
-//		UARTCharPut(UART1_BASE, (leitura>>4) & 0xFF);
 
-		if (LED < limLED/2)
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 1<<3);
-		else //if (val == 255)
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-
+		// LED blinking logic
+		if (LED < limLED/2) GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 1<<3);
+		else GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
 		if (LED++ == limLED) LED = 0;
-
-//#endif
-		/*
-		 val = (char) ((sin(LED * 2.0 * PI / 256.0) + 1.0) * 128.0);
-
-		UARTCharPut(UART0_BASE, val);
-
-		LED++;
-		if (LED%2) //if (val == 0)
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 1<<3);
-		else //if (val == 255)
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-			*/
 	}
-	/*
-	while(1)
-	{
-
-		// Turn on the LED
-		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, LED);
-		// Delay for a bit
-		SysCtlDelay(2000000);
-		// Cycle through Red, Green and Blue LEDs
-		if (LED == 8) {LED = 2;} else {LED = LED*2;}
-
-		int i = 0;
-
-		for (i=0; i < 2000000/100; i++) {
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 1<<3);
-			SysCtlDelay(2000000*5/100);
-			GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-			SysCtlDelay(2000000*95/100);
-		}
-
-		GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_3, 0);
-		SysCtlDelay(2000000);
-	}
-	 */
 }
